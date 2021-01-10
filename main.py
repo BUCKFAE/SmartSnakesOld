6664bc9f36a8cce9e37d4a6c3042d4fdcc7c4105
Julian Schubert <schubertjulian1999@icloud.com>
2021-01-10 11:04:05 +0100

    Finished quick and dirty, CLEANUP REQUIRED

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Main"""
 
+import random
 import pygame
 from pygame.locals import KEYDOWN, K_ESCAPE, QUIT, K_UP, K_LEFT, K_RIGHT
 
@@ -26,52 +27,117 @@ def main():
     # Variable that controls the main loop
     running = True
 
+    # Creating initial population
+    brains = []
+
+    for current_id in range(0, 100):
+        current_brain_moves = []
+        print("Creating brain {}".format(current_id))
+        for _ in range(0, 10_000):
+            current_brain_moves.append(random.choice(list(RelativeDirections)))
+        current_brain = {'id': current_id, 'moves': current_brain_moves, 'score': 0}
+        brains.append(current_brain)
+
+
     # Main loop
-    while running:
+    for current_generation in range(0, 1_000_000):
 
-        # Default move
-        next_move = RelativeDirections.AHEAD
+        print("Current Generation: {}".format(current_generation))
+        
+        # Crossover
+        if not current_generation == 0:
+            new_pool = []
+            avg = 0
+            for current_brain in brains:
+                avg += current_brain['score']
+                for i in range(0, current_brain['score']):
+                    new_pool.append(current_brain)
+            avg /= len(brains)
 
-        # Event queue
-        for event in pygame.event.get():
+            print("Avg: {}".format(avg))
+            brains_to_create = len(brains)
+            brains = []
+            for current_brain in range(0, brains_to_create):
+                break_point = random.randint(0, 10_000)
+                first_brain = random.choice(new_pool)
+                second_brain = random.choice(new_pool)
+                new_moves = first_brain['moves'][:break_point] + second_brain['moves'][break_point:]
+                new_brain = {'id': current_brain, 'moves': new_moves, 'score': 0}
+                brains.append(new_brain)
 
-            # Key was pressed
-            if event.type == KEYDOWN:
 
-                # Escape key was pressed
-                if event.key == K_ESCAPE:
-                    running = False
+        for current_brain in brains:
 
-                # Movement
-                if event.key == K_UP:
-                    next_move = RelativeDirections.AHEAD
-                if event.key == K_LEFT:
-                    next_move = RelativeDirections.LEFT
-                if event.key == K_RIGHT:
-                    next_move = RelativeDirections.RIGHT
+            alive = True
+            snake = Snake()
 
-            # Quit
-            if event.type == QUIT:
-                running = False
+            for current_move in current_brain['moves']:
 
-        # Updating the snake position
-        snake.update(next_move)
+                # Snake died last move
+                if not alive:
+                    break
 
-        # Setting backround color
-        screen.fill(("#16a085"))
+                # Getting the next move
+                next_move = current_move
+     
+                # Exit conditiion - TODO
+                if not running:
+                    break
 
-        # Getting the different parts of the snake
-        snake_head = snake.get_head()
-        snake_body = snake.get_body()
+                # Event queue
+                for event in pygame.event.get():
 
-        # Drawing the snake
-        screen.blit(snake_head[0], snake_head[1])
-        for snake_body_piece in snake_body:
-            screen.blit(snake_body_piece[0], snake_body_piece[1])
+                    # Key was pressed
+                    if event.type == KEYDOWN:
 
-        # Pushing changes to screen
-        pygame.display.flip()
-        clock.tick(15)
+                        # Escape key was pressed
+                        if event.key == K_ESCAPE:
+                            running = False
+
+                        # Movement
+                        if event.key == K_UP:
+                            next_move = RelativeDirections.AHEAD
+                        if event.key == K_LEFT:
+                            next_move = RelativeDirections.LEFT
+                        if event.key == K_RIGHT:
+                            next_move = RelativeDirections.RIGHT
+
+                    # Quit
+                    if event.type == QUIT:
+                        running = False
+
+                # Updating the snake position
+                current_brain['score'] += snake.update(next_move)
+
+                # Setting backround color
+                screen.fill(("#16a085"))
+
+                # Getting the different parts of the snake
+                snake_head = snake.get_head()
+                snake_body = snake.get_body()
+                snake_food = snake.get_food()
+
+                # Drawing the snake
+                screen.blit(snake_head[0], snake_head[1])
+                for snake_body_piece in snake_body:
+                    screen.blit(snake_body_piece[0], snake_body_piece[1])
+
+                # Drawing the food
+                screen.blit(snake_food[0], snake_food[1])
+
+                # Checking if the snake hit itself
+                if any([snake_head[1].colliderect(body_piece[1]) for body_piece in snake_body]):
+                    alive = False
+
+                # Checking if the snake is out of bounds
+                if snake_head[1].x < 0 or snake_head[1].y < 0 \
+                        or snake_head[1].center[0] > SCREEN_WIDTH \
+                        or snake_head[1].center[1] > SCREEN_HEIGHT:
+                    alive = False
+
+                # Pushing changes to screen
+                pygame.display.flip()
+                clock.tick(10000000000)
 
 if __name__ == "__main__":
     print("SmartSnakes - by Buckfae")
--- snake.py
+++ snake.py
@@ -1,6 +1,8 @@
 """Snake"""
+import random
 import pygame
 from directions import AbsoluteDirections, get_absolute_direction
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT, RANOM_SEED
 
 class Snake(pygame.sprite.Sprite):
     """Represents the snake on the board"""
@@ -13,12 +15,27 @@ class Snake(pygame.sprite.Sprite):
         self.surfaces = []
         self.rectangles = []
 
+        # Stores food positons
+        self.food_positions = []
+
+        random.seed(RANOM_SEED)
+
+        # Creating the food
+        for _ in range(0, 100):
+            randx = round(random.randint(0, SCREEN_WIDTH), -1)
+            randy = round(random.randint(0, SCREEN_HEIGHT), -1)
+            self.food_positions.append((randx, randy))
+
+        # Id of the current food
+        self.current_food = 0
+
+        self.create_food_at(self.food_positions[0])
+
         # 0 -> UP, 1 -> RIGHT, 2 -> DOWN, 3 -> LEFT
         self.facing = AbsoluteDirections.DOWN
-        print(self.facing)
 
         # Crating the body pieces
-        for current_id in range(0, 5):
+        for current_id in range(3, -1, -1):
             current_surface = pygame.Surface((10, 10))
             current_surface.fill(("#272b30"))
             self.surfaces.append(current_surface)
@@ -26,25 +43,41 @@ class Snake(pygame.sprite.Sprite):
 
     def update(self, relative_direction):
         """Updates Snake position"""
-        del self.rectangles[0]
-        self.rectangles.insert(len(self.rectangles) - 1, self.rectangles[-1].copy())
+
+        score = 0
+
+        # If we are on food tile
+        if self.rectangles[0].colliderect(self.food_rectangle):
+            self.current_food += 1
+            self.create_food_at(self.food_positions[self.current_food])
+            self.surfaces.append(pygame.Surface((10, 10)))
+            self.surfaces[-1].fill("#272b30")
+            score += 500
+        else:
+            del self.rectangles[-1]
+            score += 1
+
+        # Extending the snake
+        self.rectangles.insert(0, self.rectangles[0].copy())
 
         # Getting the absolute direction of the move
         absolute_move = get_absolute_direction(self.facing, relative_direction)
 
         # Moving the snake
         if absolute_move == AbsoluteDirections.UP:
-            self.rectangles[-1].move_ip(0, -10)
+            self.rectangles[0].move_ip(0, -10)
             self.facing = AbsoluteDirections.UP
         if absolute_move == AbsoluteDirections.RIGHT:
-            self.rectangles[-1].move_ip(10, 0)
+            self.rectangles[0].move_ip(10, 0)
             self.facing = AbsoluteDirections.RIGHT
         if absolute_move == AbsoluteDirections.DOWN:
-            self.rectangles[-1].move_ip(0, 10)
+            self.rectangles[0].move_ip(0, 10)
             self.facing = AbsoluteDirections.DOWN
         if absolute_move == AbsoluteDirections.LEFT:
-            self.rectangles[-1].move_ip(-10, 0)
+            self.rectangles[0].move_ip(-10, 0)
             self.facing = AbsoluteDirections.LEFT
+
+        return score
 
     def get_head(self):
         """Returns the head of the snake"""
@@ -53,3 +86,15 @@ class Snake(pygame.sprite.Sprite):
     def get_body(self):
         """Returns the body of the snake"""
         return list(zip(self.surfaces[1:], self.rectangles[1:]))
+
+    def get_food(self):
+        """Returns the food"""
+        return self.food_surface, self.food_rectangle
+
+    def create_food_at(self, food_position):
+        """Sets the food to be at the given coordinates"""
+        self.food_surface = pygame.Surface((10, 10))
+        self.food_surface.fill("#ed0f46")
+        foodx = food_position[0]
+        foody = food_position[1]
+        self.food_rectangle = self.food_surface.get_rect(center = (foodx, foody))
